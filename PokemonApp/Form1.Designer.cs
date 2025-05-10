6c5c086e7830791aa4d47c51c0ba0bc1dddd4154
Abner Mateo <amateroh@miumg.edu.gt>
2025-05-09 21:52:15 -0600

    tareaapi

---
+++ PokemonApp/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace PokemonApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtBusqueda = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.picImagen = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.lblTitulo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtBusqueda
+            // 
+            this.txtBusqueda.Location = new System.Drawing.Point(167, 98);
+            this.txtBusqueda.Name = "txtBusqueda";
+            this.txtBusqueda.Size = new System.Drawing.Size(171, 20);
+            this.txtBusqueda.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(35, 93);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 25);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "\"Buscar\"";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(35, 124);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 25);
+            this.btnLimpiar.TabIndex = 3;
+            this.btnLimpiar.Text = "\"Limpiar\"";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(51, 171);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(35, 13);
+            this.lblNombre.TabIndex = 4;
+            this.lblNombre.Text = "label1";
+            this.lblNombre.Click += new System.EventHandler(this.lblNombre_Click);
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Location = new System.Drawing.Point(51, 210);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(35, 13);
+            this.lblPeso.TabIndex = 5;
+            this.lblPeso.Text = "label1";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(51, 245);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(35, 13);
+            this.lblAltura.TabIndex = 6;
+            this.lblAltura.Text = "label1";
+            // 
+            // picImagen
+            // 
+            this.picImagen.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picImagen.Location = new System.Drawing.Point(177, 153);
+            this.picImagen.Name = "picImagen";
+            this.picImagen.Size = new System.Drawing.Size(145, 105);
+            this.picImagen.TabIndex = 7;
+            this.picImagen.TabStop = false;
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Font = new System.Drawing.Font("Ravie", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.Red;
+            this.lblTitulo.Location = new System.Drawing.Point(4, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(606, 65);
+            this.lblTitulo.TabIndex = 9;
+            this.lblTitulo.Text = "\"Busqueda de Pokemon\"";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTitulo.Click += new System.EventHandler(this.lblTitulo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 379);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.picImagen);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBusqueda);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picImagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBusqueda;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.PictureBox picImagen;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
+
---
+++ PokemonApp/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace PokemonApp
+{
+    public partial class Form1 : Form
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void lblNombre_Click(object sender, EventArgs e)
+        {
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private async void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string pokemonName = txtBusqueda.Text.ToLower().Trim();
+            if (string.IsNullOrEmpty(pokemonName))
+            {
+                MessageBox.Show("Por favor, ingrese un nombre de Pokémon.");
+                return;
+            }
+
+            await ObtenerDatosPokemonAsync(pokemonName);
+        }
+
+        private async Task ObtenerDatosPokemonAsync(string nombre)
+        {
+            try
+            {
+                string url = $"https://pokeapi.co/api/v2/pokemon/{nombre}";
+                HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("No se encontró el Pokémon o hubo un error.");
+                    return;
+                }
+
+                string json = await response.Content.ReadAsStringAsync();
+                JObject datos = JObject.Parse(json);
+
+                lblNombre.Text = $"Nombre: {datos["name"]}";
+                lblPeso.Text = $"Peso: {datos["weight"]} kg";
+                lblAltura.Text = $"Altura: {datos["height"]} m";
+
+                picImagen.ImageLocation = datos["sprites"]["front_default"].ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al obtener datos: {ex.Message}");
+            }
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtBusqueda.Clear(); // Limpiar el campo de búsqueda
+            lblNombre.Text = "Nombre: ";
+            lblPeso.Text = "Peso: ";
+            lblAltura.Text = "Altura: ";
+            picImagen.ImageLocation = null; // Eliminar la imagen
+        }
+
+
+
+        private void lblTitulo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picPokeball_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+
+
